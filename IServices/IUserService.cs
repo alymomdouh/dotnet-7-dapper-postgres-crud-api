0cf4c99c5e3689dbf2c520b02a6b38559cd0447d
Aly Mamdouh <aly.momdouh@gmail.com>
2023-07-02 12:33:38 +0300

    add UserService

---
+++ IServices/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using dotnet_7_dapper_postgres_crud_api.Entities;
+using dotnet_7_dapper_postgres_crud_api.Models.Users;
+
+namespace dotnet_7_dapper_postgres_crud_api.IServices
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<User>> GetAll();
+        Task<User> GetById(int id);
+        Task Create(CreateRequest model);
+        Task Update(int id, UpdateRequest model);
+        Task Delete(int id);
+    }
+}
