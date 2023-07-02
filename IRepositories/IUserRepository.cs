162cca4269c6e0bf1a6e7eb6991b2d7e6ccb6b1a
Aly Mamdouh <aly.momdouh@gmail.com>
2023-07-02 12:31:56 +0300

    add UserRepository

---
+++ IRepositories/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using dotnet_7_dapper_postgres_crud_api.Entities;
+
+namespace dotnet_7_dapper_postgres_crud_api.IRepositories
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<User>> GetAll();
+        Task<User> GetById(int id);
+        Task<User> GetByEmail(string email);
+        Task Create(User user);
+        Task Update(User user);
+        Task Delete(int id);
+    }
+}
