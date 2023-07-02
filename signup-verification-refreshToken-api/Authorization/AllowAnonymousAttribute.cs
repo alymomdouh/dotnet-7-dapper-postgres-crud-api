576680899bc3e8b38a391b58b9e7873be83abc38
Aly Mamdouh <aly.momdouh@gmail.com>
2023-07-02 19:14:27 +0300

    add JwtMiddleware and AuthorizeAttribute

---
+++ signup-verification-refreshToken-api/Authorization/AllowAnonymousAttribute.cs
@@ -0,0 +1,7 @@
+﻿namespace signup_verification_refreshToken_api.Authorization
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class AllowAnonymousAttribute : Attribute
+    {
+    }
+}
---
+++ signup-verification-refreshToken-api/Authorization/JwtMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Options;
+using signup_verification_refreshToken_api.DbContext;
+using signup_verification_refreshToken_api.Helpers;
+
+namespace signup_verification_refreshToken_api.Authorization
+{
+    public class JwtMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly AppSettings _appSettings;
+
+        public JwtMiddleware(RequestDelegate next, IOptions<AppSettings> appSettings)
+        {
+            _next = next;
+            _appSettings = appSettings.Value;
+        }
+
+        public async Task Invoke(HttpContext context, DataContext dataContext, IJwtUtils jwtUtils)
+        {
+            var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+            var accountId = jwtUtils.ValidateJwtToken(token);
+            if (accountId != null)
+            {
+                // attach account to context on successful jwt validation
+                context.Items["Account"] = await dataContext.Accounts.FindAsync(accountId.Value);
+            }
+
+            await _next(context);
+        }
+    }
+}
