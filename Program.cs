8689a37ce9ac43668e74853e244db01b421caf0d
Aly Mamdouh <aly.momdouh@gmail.com>
2023-07-02 12:38:09 +0300

    inject all inside program

--- Program.cs
+++ Program.cs
@@ -1,12 +1,42 @@
 
+using dotnet_7_dapper_postgres_crud_api.DbContext;
+using dotnet_7_dapper_postgres_crud_api.Helpers;
+using dotnet_7_dapper_postgres_crud_api.IRepositories;
+using dotnet_7_dapper_postgres_crud_api.IServices;
+using dotnet_7_dapper_postgres_crud_api.Repositories;
+using dotnet_7_dapper_postgres_crud_api.Services;
+using System.Text.Json.Serialization;
+
 namespace dotnet_7_dapper_postgres_crud_api
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            // add services to DI container
+            {
+                var services = builder.Services;
+                var env = builder.Environment;
+                services.AddCors();
+                services.AddControllers().AddJsonOptions(x =>
+                {
+                    // serialize enums as strings in api responses (e.g. Role)
+                    x.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
 
+                    // ignore omitted parameters on models to enable optional params (e.g. User update)
+                    x.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+                });
+                services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+                // configure strongly typed settings object
+                services.Configure<DbSettings>(builder.Configuration.GetSection("DbSettings"));
+
+                // configure DI for application services
+                services.AddSingleton<DataContext>();
+                services.AddScoped<IUserRepository, UserRepository>();
+                services.AddScoped<IUserService, UserService>();
+            }
             // Add services to the container.
 
             builder.Services.AddControllers();
@@ -23,10 +53,28 @@ namespace dotnet_7_dapper_postgres_crud_api
                 app.UseSwaggerUI();
             }
 
+            // ensure database and tables exist
+            {
+                using var scope = app.Services.CreateScope();
+                var context = scope.ServiceProvider.GetRequiredService<DataContext>();
+                await context.Init();
+            }
+
+            // configure HTTP request pipeline
+            {
+                // global cors policy
+                app.UseCors(x => x
+                    .AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+
+                // global error handler
+                app.UseMiddleware<ErrorHandlerMiddleware>(); 
+            }
+
             app.UseHttpsRedirection();
 
-            app.UseAuthorization();
-
+            app.UseAuthorization(); 
 
             app.MapControllers();
 
