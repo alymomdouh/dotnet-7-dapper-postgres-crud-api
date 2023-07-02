609ee8f4c03a4f4a9871e2a54b5bd7d1956050c9
Aly Mamdouh <aly.momdouh@gmail.com>
2023-07-02 12:30:18 +0300

    add dbcontext and connection string and entities and map connection sttring to class

---
+++ Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+using dotnet_7_dapper_postgres_crud_api.Enums;
+
+namespace dotnet_7_dapper_postgres_crud_api.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public Role Role { get; set; }
+
+        [JsonIgnore]
+        public string? PasswordHash { get; set; }
+    }
+}
---
+++ Helpers/DbSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace dotnet_7_dapper_postgres_crud_api.Helpers
+{
+    public class DbSettings
+    {
+        public string? Server { get; set; }
+        public string? Database { get; set; }
+        public string? UserId { get; set; }
+        public string? Password { get; set; }
+    }
+}
